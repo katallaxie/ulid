5773a8d9b16a416e2f15aa1fcebbf7237edf9d29
Sebastian Döll <sebastian@katallaxie.me>
2023-09-24 17:36:29 +0000

    wip: setup structure

--- lib/src/ulid_base.dart
+++ lib/src/ulid_base.dart
@@ -1,6 +1,24 @@
-// TODO: Put public facing types in this file.
+import 'dart:math';
+import 'dart:typed_data';
 
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+class Ulid {
+  final Uint8List _bytes;
+
+  Ulid._(this._bytes) {
+    assert(_bytes.length == 16);
+  }
+
+  factory Ulid() {
+    final bytes = Uint8List(16);
+    final random = Random.secure();
+
+    for (var i = 0; i < 16; i++) {
+      bytes[i] = random.nextInt(256);
+    }
+
+    return Ulid._(bytes);
+  }
+
+  @override
+  int get hashCode => _bytes.join().hashCode;
 }
